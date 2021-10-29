7f8c189f1ef79e4dc5c9cd450ae258437afba0bc
Mahmoud Abdelrahman <47402937+JS-player@users.noreply.github.com>
2021-10-29 13:40:18 +0200

    I

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const inputListner = function () {
     e.preventDefault();
     const inputVal = input.value;
     if (!inputVal || inputVal.length < 1) return;
-   input.value = ' ';
+    input.value = " ";
     GetData(inputVal);
   });
 };
@@ -28,12 +28,30 @@ const GetData = (src) => {
     Sweet_Shirt_Men_Black:
       src +
       "/ra,sweatshirt,x3102,101010:01c5ca27c6,front-c,451,550,1050,1050-bg,f8f8f8.u3.jpg",
+    Hoodie_Men_White:
+      src +
+      "/ssrco,mhoodie,mens,fafafa:ca443f4786,front,square_product,x900-bg,f8f8f8.1.jpg",
+    Hoodie_Men_Black:
+      src +
+      "/ssrco,mhoodie,mens,101010:01c5ca27c6,front,square_product,x900-bg,f8f8f8.1.jpg",
+    T_Shirt_Women_White:
+      src +
+      "/ssrco,classic_tee,womens,fafafa:ca443f4786,front_alt,square_product,900x900.u1.jpg",
+    T_Shirt_Women_Black:
+      src +
+      "/ssrco,classic_tee,womens,101010:01c5ca27c6,front_alt,square_product,900x900.u1.jpg",
+    Hoodie_Women_White:
+      src +
+      "/ssrco,mhoodie,womens,fafafa:ca443f4786,front,square_product,x900-bg,f8f8f8.1u1.jpg",
+    Hoodie_Women_Black:
+      src +
+      "/ssrco,mhoodie,womens,101010:01c5ca27c6,front,square_product,x900-bg,f8f8f8.1.jpg",
   };
   resArr = [];
   for (const key in urlsObj) {
     const res = `<div class="result">
-    <h3>${key}✅ </h3>
-    <a target="blank" href="${urlsObj[key]}">Get Image</a>
+    <h3>${key} </h3>
+    <img src="${urlsObj[key]}" alt="">
   </div>`;
     resArr.push(res);
   }
